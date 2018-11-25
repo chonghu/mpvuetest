794b601507f9ccf27e271aca4d5fa3ebf08178ff
cf_hc1980 <cf_hc1980@aliyun.com>
2018-11-25 22:37:28 +0800

    main.js格式化

--- src/main.js
+++ src/main.js
@@ -3,32 +3,32 @@ import App from './App'
 import kyutil from './utils'
 
 Vue.config.productionTip = false
-//全局变量
+// 全局变量
 Vue.prototype.globalData = {}
 App.mpType = 'app'
 
-var Fly = require("flyio/dist/npm/wx")
-var fly = new Fly
-//添加拦截器
-fly.interceptors.request.use((request)=>{
+var Fly = require('flyio/dist/npm/wx')
+var fly = new Fly()
+// 添加拦截器
+fly.interceptors.request.use((request) => {
   request.body.appId = 'wxc885092dbd66a397'
   // 用户的openId在获取之后添加到全局变量中如果存在，我们将它添加到请求参数里面
-  let openId = Vue.prototype.globalData.openId;
-  if(openId){
+  let openId = Vue.prototype.globalData.openId
+  if (openId) {
     request.body.openId = openId
   }
 }
-,(config,promise)=>{
-  //给所有请求添加自定义header
-  config.headers["X-Tag"]="flyio";
-  return config;
+  , (config, promise) => {
+  // 给所有请求添加自定义header
+  config.headers['X-Tag'] = 'flyio'
+  return config
 })
-//配置请求基地址
+// 配置请求基地址
 fly.config.baseURL = 'https://a.9kylin.cn/'
 
-Vue.prototype.$kyutil = kyutil;
-//添加全局配置、拦截器等
-Vue.prototype.$http = fly //将fly实例挂在vue原型上
+Vue.prototype.$kyutil = kyutil
+// 添加全局配置、拦截器等
+Vue.prototype.$http = fly // 将fly实例挂在vue原型上
 
 const app = new Vue(App)
 app.$mount()
